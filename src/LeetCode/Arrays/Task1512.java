7df7f3d1358c8bc25fddf88227eb8524174628cc
Gunay A. Cafarli <Gunay.Cafarli@kapitalbank.az>
2022-01-21 11:36:55 +0400

    Added task1512

---
+++ src/LeetCode/Arrays/Task1512.java
@@ -0,0 +1,17 @@
+package LeetCode.Arrays;
+
+public class Task1512 {
+    public static void main(String[] args) {
+        int[] nums = new int[]{1, 1, 1, 1};
+        int numIdenticalPairs = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                if (nums[i] == nums[j] && i < j)
+                    numIdenticalPairs++;
+            }
+        }
+
+        System.out.println(numIdenticalPairs);
+    }
+}
