f09e6bec6c75dac7f8adac59ea0ce53b2adab60a
Gunay-Jafar <cafarligunay06@gmail.com>
2022-02-02 19:55:23 +0400

    Added task 1859

--- src/LeetCode/String/Task1859.java
+++ src/LeetCode/String/Task1859.java
@@ -1,13 +1,17 @@
 package LeetCode.String;
 
-import java.util.Scanner;
-
+//https://leetcode.com/problems/sorting-the-sentence/
 public class Task1859 {
     public static void main(String[] args) {
         String str = "Myself2 Me1 I4 and3";
-        String[] newStr = str.split("\\s+");
-        for (int i = 0; i < newStr.length; i++) {
-            System.out.println(newStr[i]);
+        String[] strings = str.split("\\s+");
+        String[] result = new String[strings.length];
+
+        for (int i = 0; i < strings.length; i++) {
+            int index = Integer.valueOf(strings[i].substring(strings[i].length() - 1)) - 1;
+            result[index] = strings[i].substring(0, strings[i].length() - 1);
+
         }
+        System.out.println(String.join(" ",result));
     }
 }
