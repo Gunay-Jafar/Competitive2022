99e45948fe7234d0adc85a81a3be8658294fc511
Gunay-Jafar <cafarligunay06@gmail.com>
2022-02-17 02:29:47 +0400

    Added task 1876

---
+++ src/LeetCode/LinkedList/Task876.java
@@ -0,0 +1,34 @@
+package LeetCode.LinkedList;
+
+public class Task876 {
+//    /**
+//     * Definition for singly-linked list.
+//     * public class ListNode {
+//     *     int val;
+//     *     ListNode next;
+//     *     ListNode() {}
+//     *     ListNode(int val) { this.val = val; }
+//     *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+//     * }
+//     */
+//    class Solution {
+//        public ListNode middleNode(ListNode head) {
+//            ListNode node = head;
+//            int size = 0;
+//
+//            while(node != null) {
+//                node = node.next; //2,3,4,5,,null
+//                size++; //1,2,3,4,5
+//            }
+//
+//            int mid = size/2; //2
+//            node = head;
+//            while(mid>0) {
+//                node= node.next;
+//                mid--;
+//            }
+//
+//            return node;
+//        }
+//    }
+}
