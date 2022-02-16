2ad881d5c8ec76dd1922c8d513f94f3d9f6931b7
Gunay-Jafar <cafarligunay06@gmail.com>
2022-02-17 01:16:21 +0400

    Added task

---
+++ src/LeetCode/LinkedList/Task1290.java
@@ -0,0 +1,13 @@
+package LeetCode.LinkedList;
+
+public class Task1290 {
+//    public static void main(String[] args) {
+//        String num = "";
+//        while(head!=null) {
+//            num = num + head.val;
+//            head = head.next;
+//        }
+//        int value = Integer.parseInt(num,2);
+//       // return value;
+//    }
+}
---
+++ src/LeetCode/Math/Task1588.java
@@ -0,0 +1,10 @@
+package LeetCode.Math;
+
+public class Task1588 {
+    public static void main(String[] args) {
+        int[] arr = new int[]{1, 4, 2, 5, 3};
+
+        for (int i = 1; i < arr.length; i = i + 2) {
+        }
+    }
+}
