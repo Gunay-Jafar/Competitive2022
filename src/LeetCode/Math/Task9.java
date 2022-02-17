7c0a0655d33b1e5fa4c54f0d5cebda73c222c9b8
Gunay-Jafar <cafarligunay06@gmail.com>
2022-02-17 23:51:17 +0400

    Added task1614

--- src/LeetCode/Math/Task9.java
+++ src/LeetCode/Math/Task9.java
@@ -1,2 +1,21 @@
-package LeetCode.Math;public class Task9 {
+package LeetCode.Math;
+
+public class Task9 {
+    public static void main(String[] args) {
+        int x = 121;
+
+        int n = x;
+        int r = 0;
+        if (x < 0) {
+            //return false;
+        }
+
+        while (x != 0) {
+            int temp = x % 10;
+            r = r * 10 + temp;
+            x = x / 10;
+        }
+
+       // return (n == r);
+    }
 }
---
+++ src/LeetCode/Stack/Task1614.java
@@ -0,0 +1,23 @@
+package LeetCode.Stack;
+
+import java.util.Stack;
+
+public class Task1614 {
+    public static void main(String[] args) {
+        String s = "";
+        Stack<Character> st = new Stack<>();
+        int max = 0, count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            max = Math.max(max, count);
+            1, 2, 3
+            if (s.charAt(i) == '(') {
+                st.push(s.charAt(i));
+                count++;// 1,2,3
+            } else if (s.charAt(i) == ')') {
+                st.pop();
+                count--;
+            }
+        }
+        // return max;
+    }
+}
